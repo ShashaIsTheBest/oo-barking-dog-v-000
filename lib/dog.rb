11178e3333c2eca503db1ef88cfc698af1c1e4b8
Shasha Yan <shashayan@gmail.com>
2021-05-15 03:45:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
   def name= (dog_name)
-    this_dogs_name = Fido
+    this_dogs_name = dog_name
   end
 
   def name
@@ -13,5 +13,5 @@ class Dog
 end
 # Your code goes here!
 fido = Dog.new
-fido.name = Fido
+fido.name = "Fido"
 fido.bark
